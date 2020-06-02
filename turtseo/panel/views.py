c0904a858e9dcb4e9d5a61ef268275045e2dab8e
arnirob <arnirob741@gmail.com>
2020-06-03 02:56:53 +0600

    added templates and views

--- turtseo/panel/views.py
+++ turtseo/panel/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 # from . models import 
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 
-# @csrf_exempt
+@csrf_exempt
 def add_data(request):
-	return HttpResponse("Data Entry")
+	print("request aise")
+	# if request.method == 'POST':
+	# 	linkid = request.POST['linkid']
+	# 	exampleFormControlFile1 = request.POST['exampleFormControlFile1']
+	# 	tagarea = request.POST['tagarea']
+	# 	niche = request.POST['niche']
+	# 	dr = request.POST['dr']
+	# 	da = request.POST['da']
+	# 	spamscore = request.POST['spamscore']
+	# 	traffic = request.POST['traffic']
+	# 	existingcost = request.POST['existingcost']
+	# 	newCost = request.POST['new Cost']
+	# 	email = request.POST['email']		
+
+	# return HttpResponse("Data Entry")
+	return render(request, 'html/index.html')
