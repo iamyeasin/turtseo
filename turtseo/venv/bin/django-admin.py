c34303dd45117a0363cb4d37d164dd7772b83f74
arnirob <arnirob741@gmail.com>
2020-06-03 01:29:51 +0600

    chudi

--- turtseo/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ar_nirob/Documents/Yeasin's Projet/turtseo/turtseo/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
