d58990ad7abad4358bcc3e8af3bd163040c5cab3
Md. Zahid Hasan Joy <hasan84848484@gmail.com>
2020-06-03 23:04:44 +0600

    Fix Static and PapaParse Issue

--- turtseo/panel/views.py
+++ turtseo/panel/views.py
@@ -15,49 +15,6 @@ def FileUpload(request):
 		fileName = str(exelFile)
 		print(fileName)
 
-		data = "C:\\Users\\hasan\\Downloads\\" + fileName
-		print(data)
-
-		with open(data, mode='r') as csv_file:
-			csv_reader = csv.reader(csv_file)
-			line_count = 0
-			for row in csv_reader:
-				if line_count == 0:
-					print(f'Column names are {", ".join(row)}')
-					line_count += 1
-
-				print(row["#"])
-				line_count += 1
-			print(f'Processed {line_count} lines.')
-
-		print("Success")
-
-
-		# print(exelData['Referring Page URL'])
-
 	# return HttpResponse("OK")
 	return render(request, 'html/index.html')
 
-
-
-
-# @csrf_exempt
-# def add_data(request):
-# 	print("request aise")
-# 	# if request.method == 'POST':
-# 	# 	linkid = request.POST['linkid']
-# 	# 	exampleFormControlFile1 = request.POST['exampleFormControlFile1']
-# 	# 	tagarea = request.POST['tagarea']
-# 	# 	niche = request.POST['niche']
-# 	# 	dr = request.POST['dr']
-# 	# 	da = request.POST['da']
-# 	# 	spamscore = request.POST['spamscore']
-# 	# 	traffic = request.POST['traffic']
-# 	# 	existingcost = request.POST['existingcost']
-# 	# 	newCost = request.POST['new Cost']
-# 	# 	email = request.POST['email']		
-
-# 	# return HttpResponse("Data Entry")
-# 	# return an object to HTML page 
-# 	# get cols
-# 	return render(request, 'html/index.html')
--- turtseo/turtseo/settings.py
+++ turtseo/turtseo/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -121,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates\html'),
+]
